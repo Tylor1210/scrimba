63e6038700c82d05ea365adfab93c0689ce5da2c
Tylor Clont <tclont@yahoo.com>
2023-09-04 10:52:45 -0400

    Scrimba | React | useState() | onClick() | Count (add/Subtract) |

---
+++ scrimba8.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+export default function App() {
+    /**
+     * Challenge: 
+     * See if you can think of a way to add 1 to the count
+     * every time the + button is clicked
+     */
+    const [count, setCount] = React.useState(0)
+    
+    function add() {
+        setCount(count + 1)
+    }
+    
+    return (
+        <div className="counter">
+            <button className="counter--minus">–</button>
+            <div className="counter--count">
+                <h1>{count}</h1>
+            </div>
+            <button className="counter--plus" onClick={add}>+</button>
+        </div>
+    )
+}
