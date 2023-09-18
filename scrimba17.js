236d63498b02e6c2d0dd4f04b51871fa39d2f76e
Tylor Clont <tclont@yahoo.com>
2023-09-17 21:06:51 -0400

    Scrimba | React | Darkmode! | props | usestate() |

---
+++ scrimba17.js
@@ -0,0 +1,24 @@
+import React from "react"
+import boxes from "./boxes"
+
+export default function App(props) {
+    const [squares, setSquares] = React.useState(boxes)
+    
+    
+    // Challenge: use a ternary to determine the backgroundColor.
+    // If darkMode is true, set it to "#222222"
+    // If darkMode is false, set it to "#cccccc"
+    
+    const styles = {
+        backgroundColor: props.darkMode ? "#222222" : "#cccccc"
+    }
+    
+    const squareElements = squares.map(square => (
+        <div style={styles} className="box" key={square.id}></div>
+    ))
+    return (
+        <main>
+            {squareElements}
+        </main>
+    )
+}
